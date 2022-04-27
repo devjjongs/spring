1811d8a96e68c06428d146de3fe34a3ac0ac4458
devjjongs <84493338+devjjongs@users.noreply.github.com>
2022-04-27 13:44:09 +0900

    Create CourseController.java
    컨트롤러를 이용하여 브라우저에 출력하기

---
+++ week01/src/main/java/com/sparta/week01/controller/CourseController.java
@@ -0,0 +1,20 @@
+package com.sparta.week01.controller;
+
+import com.sparta.week01.prac.Course;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CourseController {
+
+    @GetMapping("/courses")
+    public Course getCourses() {
+        // 코스 생성
+        Course course = new Course();
+        //변수 설정
+        course.setTitle("웹개발의 봄 스프링");
+        course.setDays(35);
+        course.setTutor("남병관");
+        return course;
+    }
+}
